e0063764c35021361f9e3ab2b7008c481ce85824
Iheaneme Gregory <iheanemegregory9@gmail.com>
2023-04-14 14:09:58 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const buttonEl = document.getElementById("buttonAdd");
+const inputEl = document.getElementById("inputField");
+
+buttonEl.addEventListener("click", function () {
+    let inputVal = inputEl.value;
+    console.log(inputVal);
+})
